36004faf68de8543c6c98bcbc4cbfa15119b0a74
smagl <smganewlife@gmail.com>
2024-02-13 11:46:45 +0200

    Added task

--- Main File.py
+++ Main File.py
@@ -2,33 +2,33 @@
 # number2 = 3
 # number3 =5
 # print(number1 + number2 + number3)
-
-
+#
+#
 # number1 = 2
 # number2 = 4
 # number3 =6
 # print(number1 * number2 * number3)
-
-
+#
+#
 # diagonal1 = 4
 # diagonal2 = 8
 # print(diagonal1 * diagonal2 / 2)
-
-
+#
+#
 # diag1 = 6
 # diag2 = 11
 # result = diag1 * diag2 / 2
 # print(result)
-
-
+#
+#
 # digit1 = int(input("Enter first number: "))
 # digit2 = int(input("Enter second number: "))
 # digit3 = int(input("Enter third number: "))
 # digit4 = int(input("Enter forth number: "))
 # result = digit1 * digit2 * digit3 * digit4
 # print(result)
-
-
+#
+#
 # number = int(input("Enter 4-digit number: "))
 # # 1246
 # num1 = number // 1000
@@ -43,10 +43,81 @@
 # result = num1 * num2 * num3 * num4
 # print(f"num1: {num1} num2: {num2} num3: {num3} num4: {num4}")
 # print(f"result: {result}")
-
+#
+# print("Hello world", "Nice to meet ya'll", "Welcome", end = " ", sep = ", ")
+# print("How are you?", end = " ")
+# print("Good, thanks")
+#
+# number1 = 1
+# number2 = 3
+# number3 =5
+# print(number1 + number2 + number3)
+#
+#
+# number1 = 2
+# number2 = 4
+# number3 =6
+# print(number1 * number2 * number3)
+#
+#
+# diagonal1 = 4
+# diagonal2 = 8
+# print(diagonal1 * diagonal2 / 2)
+#
+#
+# diag1 = 6
+# diag2 = 11
+# result = diag1 * diag2 / 2
+# print(result)
+#
+#
+# digit1 = int(input("Enter first number: "))
+# digit2 = int(input("Enter second number: "))
+# digit3 = int(input("Enter third number: "))
+# digit4 = int(input("Enter forth number: "))
+# result = digit1 * digit2 * digit3 * digit4
+# print(result)
+#
+# papapasokafkasd
+#
+# number = int(input("Enter 4-digit number: "))
+# # 1246
+# num1 = number // 1000
+# # v1
+# num2 = number % 100 // 20
+# # v2
+# num3 = number % 100 // 10
+# # v3
+# num4 = number % 10
+# # v4
+#
+# result = num1 * num2 * num3 * num4
+# print(f"num1: {num1} num2: {num2} num3: {num3} num4: {num4}")
+# print(f"result: {result}")
+#
 # print("Hello world", "Nice to meet ya'll", "Welcome", end = " ", sep = ", ")
 # print("How are you?", end = " ")
 # print("Good, thanks")
 
+# hours = int(input("Enter hours: "))
+#
+# if hours >= 12 and hours <=23:
+#     print("PM")
+# elif hours >=0 and hours <12:
+#     print("AM")
+# else:
+#     print("Incorrect hours!")
 
+# film_rating = int(input("Enter film rating"))
+#
+# if film_rating > 0 and film_rating <= 5:
+#     if film_rating == 4 or film_rating == 5:
+#         print("OK")
+#     else:
+#         print("NOT OK")
+# else: print("INCORRECT RATING")
+
+# HomeWork (12.02.2024)
+# print("Hello world")
+# ########################
 
