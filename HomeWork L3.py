fb973fd6f31d0a58ac5a259d57b934a027767908
smagl <smganewlife@gmail.com>
2024-02-15 12:07:33 +0200

    Completed Task 1

---
+++ HomeWork L3.py
@@ -0,0 +1,43 @@
+# # HomeWork3
+#
+# try:
+#     user_select = int(input("Enter a number to indicate the day of the week: "))
+#
+#     if user_select == 1:
+#         print("Sunday")
+#     elif user_select == 2:
+#         print("Monday")
+#     elif user_select == 3:
+#         print("Tuesday")
+#     elif user_select == 4:
+#         print("Wednesday")
+#     elif user_select == 5:
+#         print("Thursday")
+#     elif user_select == 6:
+#         print("Friday")
+#     elif user_select == 7:
+#         print("Saturday")
+#     else:
+#         print("Wrong input")
+#
+#     match user_select:
+#         case 1:
+#             print("Sunday")
+#         case 2:
+#             print("Monday")
+#         case 3:
+#             print("Tuesday")
+#         case 4:
+#             print("Wednesday")
+#         case 5:
+#             print("Thursday")
+#         case 6:
+#             print("Friday")
+#         case 7:
+#             print("Saturday")
+#
+# except ValueError as error:
+#     print("Please, enter an integer!")
+#     print(f"ValueError occurred: {error}")
+# except Exception as Error:
+#     print(f"Error {Error}")
