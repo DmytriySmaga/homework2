7a1abd5b6ca5f7157704a1661c3fbb7aaf76de0e
smagl <smganewlife@gmail.com>
2024-02-20 15:29:40 +0200

    Completed Task 2

--- HomeWork L4.py
+++ HomeWork L4.py
@@ -1,21 +1,32 @@
 #Lesson4
 #strings
 
-try:
-    user_writes = input("Please, enter any line: ")
+# try:
+#     user_writes = input("Please, enter any line: ")
+#
+# except Exception as Error:
+#     print(f"Error {Error}")
+#
+# letter_count = 0
+# digit_count = 0
+#
+# for letter in user_writes:
+#    if letter.isalpha():
+#        letter_count += 1
+#
+#    elif letter.isdigit():
+#        digit_count += 1
+#
+# print('Number of letters:', letter_count)
+# print('Number of digits:', digit_count)
 
-except Exception as Error:
-    print(f"Error {Error}")
+line = input("Please, type anything you want: ")
+letter = input("Enter a symbol to be found: ")
 
-letter_count = 0
-digit_count = 0
+count = 0
+for n in line:
+    if n == letter:
 
-for letter in user_writes:
-   if letter.isalpha():
-       letter_count += 1
+       count += 1
 
-   elif letter.isdigit():
-       digit_count += 1
-
-print('Number of letters:', letter_count)
-print('Number of digits:', digit_count)
+       print(f"Symbol {letter} appears {count} times in the sentence")
