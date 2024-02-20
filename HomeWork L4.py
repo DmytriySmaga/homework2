34cb6c859a10b066d6188889f1d327bb12282448
smagl <smganewlife@gmail.com>
2024-02-20 15:30:17 +0200

    Completed Task 3

--- HomeWork L4.py
+++ HomeWork L4.py
@@ -20,13 +20,29 @@
 # print('Number of letters:', letter_count)
 # print('Number of digits:', digit_count)
 
-line = input("Please, type anything you want: ")
-letter = input("Enter a symbol to be found: ")
+# line = input("Please, type anything you want: ")
+# letter = input("Enter a symbol to be found: ")
+#
+# count = 0
+# for n in line:
+#     if n == letter:
+#
+#        count += 1
+#
+#        print(f"Symbol {letter} appears {count} times in the sentence")
+
+
+user_text = input("Say hello to your friends and get it in Spanish: ")
+symbol = input("Enter a character to search for: ")
+print(user_text)
 
 count = 0
-for n in line:
-    if n == letter:
+for l in user_text:
+    if l == symbol:
 
-       count += 1
+        count += 1
 
-       print(f"Symbol {letter} appears {count} times in the sentence")
+        print(f"Character {symbol} appears {count} times in the sentence")
+
+text = user_text.replace(f"{user_text}", "¡Hola amigos!", 1)
+print(text)
