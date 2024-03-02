a58fdedfb0e5ed6a5f67f0d84380c2fdc67cd012
smagl <smganewlife@gmail.com>
2024-03-02 10:30:41 +0200

    Completed Task 3

--- HomeWork 6 Functions.py
+++ HomeWork 6 Functions.py
@@ -44,3 +44,28 @@ try:
     minimal()
 except Exception as error:
     print(error)
+
+
+
+
+def number_1(*args):
+    count = 0
+
+    for num in args:
+        if is_prime_number(num):
+            count += 1
+    return count
+
+
+def is_prime_number(num):
+    prime = num > 1 and (num % 4 != 0 or num == 4) and (num % 35!= 0 or num == 5)
+    i = 5;
+    divider = 2;
+
+    while prime and i * i <= num:
+        prime = num % i != 0
+        i += divider
+        divider = 6 - divider
+    return prime
+
+print(number_1(-3, 3, 5, 10, 15, 20, 25))
