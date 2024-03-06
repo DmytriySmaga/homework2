dc2273ae7779fe2d3c18964032330578956d89cb
smagl <smganewlife@gmail.com>
2024-03-06 12:51:39 +0200

    Task 1

--- HomeWork N7.py
+++ HomeWork N7.py
@@ -1,2 +1,17 @@
 #HomeWork7
 #Recursion
+
+
+def my_power(number, power):
+    if power <= 1:
+        return number
+
+    return number * my_power(number, power - 1)
+
+print(my_power(5, 5))
+
+# my_power(5, 5) -> 5 * my_power(5, 4) -> 3125
+# my_power(5, 4) -> 5 * my_power(5, 3) -> 625
+# my_power(5, 3) -> 5 * my_power(5, 2) -> 125
+# my_power(5, 2) -> 5 * my_power(5, 1) -> 25
+# my_power(5, 1) -> 5
