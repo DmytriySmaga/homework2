8aed50adf4cb833a0fee18bdd8c41ea403b8c6c7
smagl <smganewlife@gmail.com>
2024-03-12 23:46:17 +0200

    Task 2

--- HomeWork N8.py
+++ HomeWork N8.py
@@ -13,3 +13,14 @@ if re.findall(pattern, home_phone) is not None:
     print(home_phone)
 else:
     print("Error. Please, try again.")
+
+
+mobile_phone = "Phone number: +380982738841"
+
+pattern = r"\w+\s\w+\W\s\W\d\d\d\d\d\d\d\d\d\d\d\d"
+
+if re.search(pattern, mobile_phone) is not None:
+    print("Successful")
+    print(mobile_phone)
+else:
+    print("Error. Please, try again.")
