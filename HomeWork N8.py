fd5fd97154620c56c7548822f9d1c69e6d4d7d4e
smagl <smganewlife@gmail.com>
2024-03-12 23:45:42 +0200

    Task 1

--- HomeWork N8.py
+++ HomeWork N8.py
@@ -1,2 +1,15 @@
 # HomeWork Number8
 # Validation
+
+
+import re
+
+home_phone = "Phone number: 21-38-96"
+
+pattern = r"\w+\s\w+\W\s\d\d\D\d\d\D\d\d"
+
+if re.findall(pattern, home_phone) is not None:
+    print("Successful")
+    print(home_phone)
+else:
+    print("Error. Please, try again.")
