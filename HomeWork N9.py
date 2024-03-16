c04efce321db779dff1677de23358c24ae7c2990
smagl <smganewlife@gmail.com>
2024-03-16 10:00:35 +0200

    Task 2

--- HomeWork N9.py
+++ HomeWork N9.py
@@ -11,3 +11,20 @@ filtered_words = [word for word in words if len(word) >= 7]
 with open('File2.txt', 'w') as output_file:
 
     output_file.write('\n'.join(filtered_words))
+
+
+
+def count_words(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            text = file.read()
+            word_count = len(text.split())
+            return word_count
+    except FileNotFoundError:
+        print("File is not found.")
+        return -1
+
+file_path = "File3.txt"
+words = count_words(file_path)
+if words != -1:
+    print("The amount of words in the file:", words)
