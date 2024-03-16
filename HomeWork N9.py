7f1bdad0175b25064ba8d1b2693a4abdc3de5ed7
smagl <smganewlife@gmail.com>
2024-03-16 09:59:42 +0200

    Task 1

--- HomeWork N9.py
+++ HomeWork N9.py
@@ -1,3 +1,13 @@
 # HomeworkN9
 #Files
 
+
+with open('File1.txt', 'r') as input_file:
+    content = input_file.read()
+    words = content.split()
+
+filtered_words = [word for word in words if len(word) >= 7]
+
+with open('File2.txt', 'w') as output_file:
+
+    output_file.write('\n'.join(filtered_words))
